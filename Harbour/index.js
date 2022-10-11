3cb4db457280cff5ec2f2629f412ecae1bad4381
Resastar <hystchang@gmail.com>
2022-10-11 16:51:58 +0800

    TRPGLog
    DRYH

---
+++ Harbour/index.js
@@ -0,0 +1,19 @@
+var TITLE = "【DRYH】Harbour";
+var INDEX = [
+	{ title: "1-0", url: "【ＤＲＹＨ】Harbour-chapter1.html", },
+	{ title: "1-1", url: "【ＤＲＹＨ】Harbour-chapter1.5.html", },
+	{ title: "2-0", url: "【ＤＲＹＨ】Harbour-chapter2.html", },
+	{ title: "2-1", url: "【ＤＲＹＨ】Harbour-chapter2.5.html", },
+	{ title: "3-0", url: "【ＤＲＹＨ】Harbour-chapter3.html", },
+	{ title: "3-1", url: "【ＤＲＹＨ】Harbour-chapter3.5.html", },
+	{ title: "4-0", url: "【ＤＲＹＨ】Harbour-chapter4.html", },
+	{ title: "4-1", url: "【ＤＲＹＨ】Harbour-chapter4.5.html", },
+	{ title: "5-0", url: "【ＤＲＹＨ】Harbour-chapter5.html", },
+	{ title: "5-1", url: "【ＤＲＹＨ】Harbour-chapter5.5.html", },
+	{ title: "6-0", url: "【ＤＲＹＨ】Harbour-chapter6.html", },
+	{ title: "6-1", url: "【ＤＲＹＨ】Harbour-chapter6.5.html", },
+	{ title: "7-0", url: "【ＤＲＹＨ】Harbour-chapter7.html", },
+	{ title: "7-1", url: "【ＤＲＹＨ】Harbour-chapter7.5.html", },
+	{ title: "8-0", url: "【ＤＲＹＨ】Harbour-chapter8.html", },
+	{ title: "8-1", url: "【ＤＲＹＨ】Harbour-chapter8.5.html", },
+];
