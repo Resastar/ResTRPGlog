9331958eebdffb3f7bb98fd2cfdd093909802715
Resastar <hystchang@gmail.com>
2023-05-24 19:50:41 +0800

    Fix

--- 泥偶的伊碧娜/index.js
+++ 泥偶的伊碧娜/index.js
@@ -5,7 +5,7 @@ var INDEX = [
 	{ title: "雨驟風狂", url: "伊碧娜01.html", },
 	{ title: "暮落驚雷", url: "伊碧娜02.html", },
 	{ title: "雨盡泉臨", url: "伊碧娜03.html", },
-	{ title: "薩滿新世代", url: "伊碧娜03.5.html", },
+	{ title: "薩滿新世代", url: "伊碧娜03-5.html", },
 	{ title: "聲息", url: "伊碧娜04.html", },
 	{ title: "克蒙阿朵堂", url: "伊碧娜4.5.html", },
 	{ title: "狼嚎", url: "伊碧娜05.html", },
