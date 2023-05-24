b16b5c8d8296b4805288986395511be5fb8ef384
Resastar <hystchang@gmail.com>
2023-05-24 19:48:27 +0800

    TRPGLog
    2023/05/24

---
+++ 泥偶的伊碧娜/index.js
@@ -0,0 +1,13 @@
+var TITLE = "【FATE】泥偶的伊碧娜";
+var INDEX = [
+	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/1HuWlGaaZp_4SYOuKywDCXSKdn21BAhPICJsinZCrCDI/edit?usp=sharing", },
+	{ title: "山嵐之年", url: "伊碧娜00.html", },
+	{ title: "雨驟風狂", url: "伊碧娜01.html", },
+	{ title: "暮落驚雷", url: "伊碧娜02.html", },
+	{ title: "雨盡泉臨", url: "伊碧娜03.html", },
+	{ title: "薩滿新世代", url: "伊碧娜03.5.html", },
+	{ title: "聲息", url: "伊碧娜04.html", },
+	{ title: "克蒙阿朵堂", url: "伊碧娜4.5.html", },
+	{ title: "狼嚎", url: "伊碧娜05.html", },
+	{ title: "追亡逐北", url: "伊碧娜05.5.html", },
+];
