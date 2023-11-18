d509e993dd723b64d22b1207c4a41515abe0666f
Resastar <hystchang@gmail.com>
2023-11-18 12:04:53 +0800

    2023/11

--- 寶可夢農場/index.js
+++ 寶可夢農場/index.js
@@ -1,9 +1,12 @@
 var TITLE = "【FATE】藤藤蛇的農場大冒險";
 var INDEX = [
 	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/1XMg3m8SPPQLFRkCV6c3hJOJaFV6u0SajlSDl_Vx9GTI/edit?usp=sharing", },
-	{ title: "農場一天的開始", url: "藤藤蛇00.html", },
-	{ title: "雨中幽靈", url: "藤藤蛇01.html", },
-	{ title: "白色的外來者", url: "藤藤蛇02.html", },
-	{ title: "雨過天晴", url: "藤藤蛇03.html", },
+	{ title: "一天的開始(1)", url: "藤藤蛇00.html", },
+	{ title: "一天的開始(2)", url: "藤藤蛇01.html", },
+	{ title: "一天的開始(3)", url: "藤藤蛇02.html", },
+	{ title: "一天的開始(4)", url: "藤藤蛇03.html", },
+	{ title: "雨中幽靈", url: "藤藤蛇04.html", },
+	{ title: "白色的外來者", url: "藤藤蛇05.html", },
+	{ title: "雨過天晴", url: "藤藤蛇06.html", },
 
 ];
