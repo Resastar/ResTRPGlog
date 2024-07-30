4ae57dbad7986f4f1a4df124e55a85a8932f0c58
Resastar <hystchang@gmail.com>
2024-07-31 00:26:24 +0800

    Update index.js

--- RAR/index.js
+++ RAR/index.js
@@ -1,4 +1,4 @@
-var TITLE = "【FATE】藤藤蛇的農場大冒險";
+var TITLE = "【FATE】RAR Foundation";
 var INDEX = [
 	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/1jW3bCe5jcoEbJpVM6RK_8AJYsI_IA8f2DQOjGPxQKpY/edit?gid=16495776#gid=16495776", },
 	{ title: "導入", url: "RAR00", },
