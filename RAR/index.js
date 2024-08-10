a32e9a42acbae0946a677f9f0e81a081a96e826b
Resastar <hystchang@gmail.com>
2024-08-11 00:39:02 +0800

    02

--- RAR/index.js
+++ RAR/index.js
@@ -1,7 +1,8 @@
 var TITLE = "【FATE】RAR Foundation";
 var INDEX = [
 	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/1jW3bCe5jcoEbJpVM6RK_8AJYsI_IA8f2DQOjGPxQKpY/edit?gid=16495776#gid=16495776", },
-	{ title: "導入", url: "RAR00", },
-	{ title: "01", url: "RAR01", },
+	{ title: "00", url: "00", },
+	{ title: "01", url: "01", },
+	{ title: "02", url: "02", },
 
 ];
