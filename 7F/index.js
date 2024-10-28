21effed6122541dc0bacd665a2e82a5f45b25b4e
Resastar <hystchang@gmail.com>
2024-10-29 00:41:04 +0800

    00

--- 7F/index.js
+++ 7F/index.js
@@ -1,4 +1,4 @@
-var TITLE = "【鬧市隱俠】七分沉霧半抹月";
+var TITLE = "【鬧市隱俠】七分柳燕";
 var INDEX = [
 	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRye45EAQragi_1AHSinyU4AvXjrA7gw312gyWJGWGy_79lF-EJT1r_hAfpuu6BeJRlwRuMlVq0YjEX/pubhtml", },
 	{ title: "00", url: "00", },
