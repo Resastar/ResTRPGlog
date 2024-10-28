778239976aa87f151834835c2e2755b7623b3256
Resastar <hystchang@gmail.com>
2024-10-29 00:33:24 +0800

    00

---
+++ 7F/index.js
@@ -0,0 +1,5 @@
+var TITLE = "【鬧市隱俠】七分沉霧半抹月";
+var INDEX = [
+	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRye45EAQragi_1AHSinyU4AvXjrA7gw312gyWJGWGy_79lF-EJT1r_hAfpuu6BeJRlwRuMlVq0YjEX/pubhtml", },
+	{ title: "00", url: "00", },
+];
