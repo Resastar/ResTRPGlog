b7e1b984eb2275d6ae7fc37027a50b91c9f770db
Resastar <hystchang@gmail.com>
2024-11-07 02:36:43 +0800

    01

--- 7F/index.js
+++ 7F/index.js
@@ -2,4 +2,5 @@ var TITLE = "【鬧市隱俠】七分柳燕";
 var INDEX = [
 	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRye45EAQragi_1AHSinyU4AvXjrA7gw312gyWJGWGy_79lF-EJT1r_hAfpuu6BeJRlwRuMlVq0YjEX/pubhtml", },
 	{ title: "00", url: "00", },
+	{ title: "01", url: "01", },
 ];
