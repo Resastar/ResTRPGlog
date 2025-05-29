4598442158a1d8ddd73953549e7461849c3ea3c9
Resastar <hystchang@gmail.com>
2025-05-30 00:12:39 +0800

    01

---
+++ BM/index.js
@@ -0,0 +1,5 @@
+var TITLE = "【暗獸歌劇】";
+var INDEX = [
+	{ title: "團務區", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6XJRtXUMv61hhKfGFH-sgx6kPjmfD3f-XFHmmTX3okPTGiiRa19kn9bNrfRa-VfKBnNXnAYbhaMSl/pubhtml", },
+	{ title: "01", url: "01", },
+];
