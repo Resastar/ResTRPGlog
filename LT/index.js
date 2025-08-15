fc4c5ad50f681bcc135c927cb7f39baebc48fe89
Resastar <hystchang@gmail.com>
2025-08-16 00:42:12 +0800

    0815

---
+++ LT/index.js
@@ -0,0 +1,6 @@
+var TITLE = "【FATE】蓋斯德大陸紀行";
+var INDEX = [
+	{ title: "團務", url: "https://docs.google.com/spreadsheets/d/1-8ksZWcUCnpuZF__W8QDlRkeyBnS4_T9nJfc1okTGOY/edit?gid=1002705862#gid=1002705862", },
+	{ title: "初夢", url: "https://docs.google.com/document/d/1BT_irP6VuJAsyePHTJXER4LBMtAglqH2enj9mdW10W0/edit?usp=sharing", },
+	{ title: "索萊鎮00", url: "索萊鎮00", },
+];
