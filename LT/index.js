52527e1e64b8096aaead938631b0b6b928434a5f
Resastar <hystchang@gmail.com>
2025-08-18 00:40:02 +0800

    0818

--- LT/index.js
+++ LT/index.js
@@ -3,4 +3,5 @@ var INDEX = [
 	{ title: "團務", url: "https://docs.google.com/spreadsheets/d/1-8ksZWcUCnpuZF__W8QDlRkeyBnS4_T9nJfc1okTGOY/edit?gid=1002705862#gid=1002705862", },
 	{ title: "初夢", url: "https://docs.google.com/document/d/1BT_irP6VuJAsyePHTJXER4LBMtAglqH2enj9mdW10W0/edit?usp=sharing", },
 	{ title: "索萊鎮00", url: "索萊鎮00", },
+	{ title: "索萊鎮01", url: "索萊鎮01", },
 ];
