d85a2113377665dd963f013002b7f3f9578f02b7
Resastar <hystchang@gmail.com>
2025-08-22 15:51:50 +0800

    0822

--- LT/index.js
+++ LT/index.js
@@ -1,7 +1,9 @@
 var TITLE = "【FATE】蓋斯德大陸紀行";
 var INDEX = [
-	{ title: "團務", url: "https://docs.google.com/spreadsheets/d/1-8ksZWcUCnpuZF__W8QDlRkeyBnS4_T9nJfc1okTGOY/edit?gid=1002705862#gid=1002705862", },
-	{ title: "初夢", url: "https://docs.google.com/document/d/1BT_irP6VuJAsyePHTJXER4LBMtAglqH2enj9mdW10W0/edit?usp=sharing", },
-	{ title: "索萊鎮00", url: "索萊鎮00", },
-	{ title: "01", url: "索萊鎮01", },
+	{ title: "初始之夢：重合的命運", url: "https://docs.google.com/document/d/1BT_irP6VuJAsyePHTJXER4LBMtAglqH2enj9mdW10W0/edit?usp=sharing", },
+	{ title: "第一站：索萊鎮", members: [
+		{ title: "00：新世界", url: "索萊鎮00", },
+		{ title: "01：新生活", url: "索萊鎮01", },
+	] },
+
 ];
