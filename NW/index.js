1794e31be9d237eb7cf41225289a88e87104a229
Resastar <hystchang@gmail.com>
2026-02-22 16:59:18 +0800

    00

---
+++ NW/index.js
@@ -0,0 +1,8 @@
+var TITLE = "【神我狩】致新世界";
+var INDEX = [
+	{ title: "角卡區", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRRQvN6CR-R6BHiMJQj8vf8eUCK0d7kBsTwKYk289yBjZ50H_8SopGq0ZMis_oW7CAqPB2J8x0RXFSP/pubhtml", },
+	{ title: "導入", members: [
+		{ title: "新世界的大門", url: "01", },
+	] },
+
+];
