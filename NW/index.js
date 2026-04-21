be195a9553c48232cd493e473d878ae0f4691b6a
Resastar <hystchang@gmail.com>
2026-04-21 16:25:48 +0800

    Update index.js

--- NW/index.js
+++ NW/index.js
@@ -1,7 +1,7 @@
 var TITLE = "【神我狩】致新世界";
 var INDEX = [
 	{ title: "角卡區", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRRQvN6CR-R6BHiMJQj8vf8eUCK0d7kBsTwKYk289yBjZ50H_8SopGq0ZMis_oW7CAqPB2J8x0RXFSP/pubhtml", },
-	{ title: "導入", members: [
+	{ title: "跑團紀錄", members: [
 		{ title: "新世界的大門", url: "01", },
 		{ title: "虛妄的新生", url: "02", },
 		{ title: "疆域淪陷", url: "03", },
